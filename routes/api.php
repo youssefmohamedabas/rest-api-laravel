06e3a7c784669f7d8e6c423561e3a9b2216a659a
youssefmohamedabass <77261058+youssefmohamedabas@users.noreply.github.com>
2025-01-28 16:08:36 +0200

    Edit route for user

--- routes/api.php
+++ routes/api.php
@@ -8,23 +8,26 @@ use App\Http\Controllers\Api\V1\LoginController;
 use App\Http\Controllers\APi\V1\RelationsController;
 use App\Http\Controllers\APi\V1\TagController;
 
+
+
+
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
+
+// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
-   
     Route::get('/users', function (Request $request) {
         return $request->user();
     });
 
-    
-    Route::apiResource('user', UserController::class);
-    Route::apiResource('lessons',LessonController::class);
-    Route::apiResource('tags',TagController::class);
-    
+    // Protect all UserController routes except 'store'
+    Route::apiResource('user', UserController::class)->except(['store']);
+    Route::apiResource('lessons', LessonController::class);
+    Route::apiResource('tags', TagController::class);
 
-    
-    Route::get('users/{id}/lessons',[RelationsController::class,'userLessons'])->name('user.lesson');
-    Route::get('lessons/{id}/tags',[RelationsController::class,'lessonTags'])->name('lesson.tags');
-    Route::get('tags/{id}/lessons',[RelationsController::class,'tagLessons'])->name('tag.lessons');
- 
+    // RelationsController routes
+    Route::get('users/{id}/lessons', [RelationsController::class, 'userLessons'])->name('user.lesson');
+    Route::get('lessons/{id}/tags', [RelationsController::class, 'lessonTags'])->name('lesson.tags');
+    Route::get('tags/{id}/lessons', [RelationsController::class, 'tagLessons'])->name('tag.lessons');
 });
 
 // Public route for login
